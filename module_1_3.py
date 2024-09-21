5cd65f34145371f6dbd09f1185ec87fb9895db36
DeBach44 <dbacherikov97@mail.ru>
2024-09-21 21:16:36 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+#DeBach
+name = "Denis"
+print("Name: " + name,)
+age = 27
+print("Age: " + str(age,))
+age = int(age,)+3
+print ("New Age: " + str(age,))
+is_student = True
+print("Is Student: " + str(is_student,))
