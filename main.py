eb43841db76d006c098e678b52361a009fef1cf5
DeBach44 <dbacherikov97@mail.ru>
2024-09-25 15:54:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#DeBach
-a = 12#Количество выполненых задач
-b = 1.5#Количество затраченного времени
-c = "Python"
-d =  b/a
-print("Курс: " + c,  ", всего задач: " + str(a,) + ",")
-print("затрачено часов: " + str(b,) + ",")
-print("среднее время выполнения: " + str(d,) + ".")
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+sort_students = sorted(students)
+a = {}
+a.update({sort_students[0] :sum (grades[0]) / len (grades[0]),
+         sort_students[1] :sum (grades[1]) / len (grades[1]),
+         sort_students[2] :sum (grades[2]) / len (grades[2]),
+         sort_students[3] :sum (grades[3]) / len (grades[3]),
+         sort_students[4] :sum (grades[4]) / len (grades[4])})
+print(a)
