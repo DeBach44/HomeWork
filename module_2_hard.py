4c3e81bae6e36e1d65dc1918a58c2edf01a42d35
DeBach44 <dbacherikov97@mail.ru>
2024-10-04 19:50:57 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,37 +1,17 @@
 #DeBach
-def number():
-    while True:
-        a = int(input("Введите число: "))
-        l = []
-        for i in range(1, 21):
-            if i >= a:
-                break
-            else:
-                for j in range(i + 1, 21):
-                    if a % (i + j) == 0:
-                        l.append(i)
-                        l.append(j)
-                    elif (i + j) >= a:
-                        break
-                    else:
-                        continue
-        raise print(f"{a}: {l}")
-number()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while True:
+    a = int(input("Введите число: "))
+    l = []
+    for i in range(1,21):
+        if i >= a:
+            break
+        else:
+            for j in range(i+1,21):
+                if a % (i + j) == 0:
+                    l.append(i)
+                    l.append(j)
+                elif(i + j) >= a:
+                    break
+                else:
+                    continue
+    print(f"{a}: {l}")
