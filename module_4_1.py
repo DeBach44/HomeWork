598e0ca525503e1de52fcd1c1586a0d8d2769db8
DeBach44 <dbacherikov97@mail.ru>
2024-10-15 20:41:22 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide as divide_f
+from true_math import divide as divide_t
+
+print(divide_f(5, 2))
+print(divide_f(4, 0))
+print()
+print(divide_t(6, 2))
+print(divide_t(7, 0))
