51336b9e2583f72d48478e9436c7d56c449e815a
DeBach44 <dbacherikov97@mail.ru>
2024-11-22 00:09:44 +0300

    Add files via upload

---
+++ module_7_4.py
@@ -0,0 +1,39 @@
+#DeBach
+#module_7_4"Форматирование строк"
+
+
+
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 45.2
+result = str
+challenge_result = result
+#________________________________%_____________________________________________
+print('В команде Мастера кода участников: %s !' % team1_num )
+
+print('Итого сегодня в командах участников: %s и %s !' % (team1_num,team2_num ))
+#________________________________format()_______________________________________
+print('Команда Волшебники данных решила задач: {} !'.format(score_2))
+
+print('Волшебники данных решили задачи за {0} с !'.format(round(team2_time,2)))
+#________________________________f-строка_______________________________________
+print(f'Команды решили {score_1} и {score_2} задач.')
+
+print(f"Результат битвы: победа команды Мастера кода!")
+
+if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
+    result = f'Победа команды Мастера кода!'
+elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
+    result = f'Победа команды Волшебники Данных!'
+else:
+    result = f'Ничья!'
+
+    print(challenge_result)
+
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.')
+
