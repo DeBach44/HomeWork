df9a08d9d1191e82ba6b3bb80b1c1e9b4708118b
DeBach44 <dbacherikov97@mail.ru>
2024-11-26 09:06:01 +0300

    Add files via upload

---
+++ mmm.py
@@ -0,0 +1,23 @@
+# DeBach
+"Базовые структуры данных"
+
+# 1st program
+
+print((9 ** 0.5) * 5)
+
+# 2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a == b)
+
+# 4th program
+
+str_ = '123.456'
+float_str_ = float(str_) * 10
+int_float_ = int(float_str_) % 10
+print(int_float_)
