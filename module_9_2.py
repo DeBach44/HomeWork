367400ecf3d59a053dc0dc8fe4d00ce5d6b6ed8c
DeBach44 <dbacherikov97@mail.ru>
2024-12-03 22:20:10 +0300

    Add files via upload

--- module_9_2.py
+++ module_9_2.py
@@ -3,13 +3,13 @@
 
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
-
+#________________________________________________________________________________________________
 first_result = [len(i) for i in first_strings if len(i) >= 5]
 
 second_result = [(i, j) for i in first_strings for j in second_strings if len(i) == len(j)]
 
 third_result = {x: len(x) for x in (first_strings + second_strings) if not len(x) % 2}
-
+#_________________________________________________________________________________________________
 print(first_result)
 print(second_result)
 print(third_result)
