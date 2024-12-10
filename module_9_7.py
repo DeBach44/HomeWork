9ebf5ea06e0e9ef607172b392bf23bc70ed91139
DeBach44 <dbacherikov97@mail.ru>
2024-12-10 14:18:59 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,23 @@
+# DeBach
+# module_9_6 "Декораторы в Python"
+
+def is_prime(func):
+    def wrapper(*args):
+        f = func(*args)
+        prime = True
+        for i in range(2,f):
+            if f % i == 0:
+                prime = False
+        if prime:
+            return (f"Простое\n{f}")
+        else:
+            return (f"Составное\n{f}")
+    return wrapper
+
+@ is_prime
+def sum_three(*args):
+    sum_num = sum(args)
+    return sum_num
+
+result = sum_three(2, 3, 6)
+print(result)
